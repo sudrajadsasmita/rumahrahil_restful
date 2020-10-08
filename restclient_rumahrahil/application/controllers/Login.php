2dab9eb929aef3e570fae94ec9a5235865973931
sudrajadsasmita <sudrajad.sasmita@gmail.com>
2020-10-08 21:03:34 +0700

    update model client

--- restclient_rumahrahil/application/controllers/Login.php
+++ restclient_rumahrahil/application/controllers/Login.php
@@ -14,7 +14,11 @@ class Login extends CI_Controller
     {
         $data['user'] = $this->user->getAllUser();
         $data['title'] = "Login Siswa/Guru";
-        $this->load->view('login/index', $data);
+        if ($this->input->post('email') === null) {
+            $this->load->view('login/index', $data);
+        } else {
+            $this->_login();
+        }
     }
 
     public function registrasi()
@@ -22,5 +26,31 @@ class Login extends CI_Controller
         $data['user'] = $this->user->getAllUser();
         $data['title'] = "Daftar Siswa/Guru baru";
         $this->load->view('login/registrasi', $data);
+    }
+    private function _login()
+    {
+        $email = htmlspecialchars($this->input->post('email'));
+        $password = $this->input->post('password');
+        $user = $this->user->getAllUser();
+        if ($user) {
+            if ($password == $user['password']) {
+                if ($user['role_id'] == 1) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('dashboard');
+                } else {
+                    echo "halaman siswa/guru (masih tahap pengambangan)";
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password anda salah</div>');
+                redirect('login');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email anda belum terdaftar</div>');
+            redirect('login');
+        }
     }
 }
--- restclient_rumahrahil/application/models/User_model.php
+++ restclient_rumahrahil/application/models/User_model.php
@@ -4,13 +4,32 @@ use GuzzleHttp\Client;
 
 class User_model extends CI_model
 {
+    private $_client;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->_client = new Client([
+            'base_uri' => 'http://localhost/rumahrahil_restful/restserver_rumahrahil/api/Admin_api/'
+        ]);
+    }
     public function getAllUser()
     {
-        $client = new Client();
-
-        $response = $client->request('GET', 'http://localhost/rumahrahil_restful/restserver_rumahrahil/api/Admin_api/User_api', [
+        $response = $this->_client->request('GET', 'User_api', [
             'query' => [
                 'rahil_key' => 'rumahrahileducation'
+            ]
+        ]);
+        $result = json_decode($response->getBody()->getContents(), true);
+
+        return $result['data'];
+    }
+
+    public function getUserWhereId($id)
+    {
+        $response = $this->_client->request('GET', 'User_api', [
+            'query' => [
+                'rahil_key' => 'rumahrahileducation',
+                'id_user' => $id
             ]
         ]);
         $result = json_decode($response->getBody()->getContents(), true);
